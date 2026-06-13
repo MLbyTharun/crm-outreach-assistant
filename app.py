bf5e5e7abf36181a20464c1b67f858c22f98bcb0
tharun <tarun.pc069@gmail.com>
2026-06-13 23:58:56 +0530

    added few features to home page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(page_title="FollowUp AI",page_icon="🤖",layout="wide")
 
@@ -13,9 +14,13 @@ col_up,col_temp =st.columns([3,2])
 with col_up:
     uploaded = st.file_uploader("Upload your CSV",type=["csv"])
     if uploaded:
+        file_key = f"{uploaded.name}_{uploaded.size}"
+        if file_key != st.session_state.last_key:
+            try:
+                if not {"name", "contact"}.issubset(set(raw.columns.str.lower().str.strip())):
+                    st.error("file must contain name and contact")
+                else:
+                    st.session_state.last_key = file_key
+                    st.session_state.name     = uploaded.name
 
 
-with col_temp:
-    temp =st.download_button("download template CSV")
-    if temp:
-        # add a sampl csv kinda here
